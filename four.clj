66a3d1dcaaf60b7c07530d77217dd888334eca8b
Hakan Raberg <hraberg@yahoo.se>
2012-08-22 00:11:38 +0100

    Refactoring back to readability

--- four.clj
+++ four.clj
@@ -1,44 +1,54 @@
 (ns four
   (import [javax.swing JFrame]
-          [java.awt Color Frame Canvas Dimension]))
+          [java.awt Color Canvas Dimension])
+  (:gen-class))
 
-(def width 800)
-(def height 600)
+(def w 800)
+(def h 600)
 (def hz 50)
 
-(defn draw [g width height]
+(defn frame [g w h]
   (doto g
     (.setColor (Color. (rand-int 0xffffff)))
-    (.fillRect 0 0 width height)
+    (.fillRect 0 0 w h)
     (.setColor Color/WHITE)
     (.drawString "4k.clj" 20 30)))
 
-(defn run [canvas]
+(defn draw [c]
+  (let [b (.getBufferStrategy c)
+        [w h] [(.getWidth c) (.getHeight c)]
+        g (.getDrawGraphics b)]
+    (try
+      (frame g w h)
+      (finally
+       (.dispose g)))
+    (when-not (.contentsLost b)
+      (.show b))))
+
+(defn run [c]
   (loop [t (System/nanoTime)]
     (try
-      (let [bs (.getBufferStrategy canvas)
-            [width height] [(.getWidth canvas) (.getHeight canvas)]]
-        (doto (.getDrawGraphics bs) (draw width height) .dispose)
-        (.show bs)
-        (let [delta (- (System/nanoTime) t)
-              frame-length (/  1000000000 hz)]
-          (when (<  delta frame-length)
-            (Thread/sleep (/ frame-length 1000000)))))
-      (catch Exception e (println e)))
-    (when (.isShowing canvas)
+      (draw c)
+      (let [d (- (System/nanoTime) t)
+            l (/ 1000000000 hz)]
+        (when (<  d l)
+          (Thread/sleep (/ l 1000000))))
+      (catch Exception e
+        (println e)))
+    (when (.isShowing c)
       (recur (System/nanoTime)))))
 
-(defn -main []
-  (let [frame (doto (JFrame.)
-                (.setResizable false)
-                (.setVisible true)
-                (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE))
-        canvas (Canvas.)]
-    (doto (.getContentPane frame)
-      (.setPreferredSize (Dimension. width height))
-      (.add canvas))
-    (.pack frame)
-    (doto canvas
+(defn -main [& args]
+  (let [f (doto (JFrame.)
+            (.setResizable false)
+            (.setVisible true)
+            (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE))
+        c (Canvas.)]
+    (doto (.getContentPane f)
+      (.setPreferredSize (Dimension. w h))
+      (.add c))
+    (.pack f)
+    (doto c
       (.setIgnoreRepaint true)
       (.createBufferStrategy 2))
-    (send-off (agent canvas) run)))
+    (send-off (agent c) run)))
