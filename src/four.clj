1df84433fb63277633463656184bea33fcdd4d19
Hakan Raberg <hraberg@yahoo.se>
2012-08-22 01:27:08 +0100

    Overall restructuring and build script

--- src/four.clj
+++ src/four.clj
@@ -7,38 +7,36 @@
 (def h 600)
 (def hz 50)
 
-(defn frame [g w h]
+(defn frame [g]
   (doto g
     (.setColor (Color. (rand-int 0xffffff)))
     (.fillRect 0 0 w h)
     (.setColor Color/WHITE)
     (.drawString "4k.clj" 20 30)))
 
-(defn draw [c]
+(defn draw [f c]
   (let [b (.getBufferStrategy c)
-        [w h] [(.getWidth c) (.getHeight c)]
         g (.getDrawGraphics b)]
     (try
-      (frame g w h)
+      (f g)
       (finally
        (.dispose g)))
     (when-not (.contentsLost b)
       (.show b))))
 
-(defn run [c]
-  (loop [t (System/nanoTime)]
+(defn run [f c]
+  (let [t (System/nanoTime)]
     (try
-      (draw c)
-      (let [d (- (System/nanoTime) t)
+      (let [_ (f c)
+            d (- (System/nanoTime) t)
             l (/ 1000000000 hz)]
         (when (<  d l)
-          (Thread/sleep (/ l 1000000))))
+          (Thread/sleep (/ (- l d) 1000000)))
+        {:duration d})
       (catch Exception e
-        (println e)))
-    (when (.isShowing c)
-      (recur (System/nanoTime)))))
+        (println e)))))
 
-(defn -main [& args]
+(defn start []
   (let [f (doto (JFrame.)
             (.setResizable false)
             (.setVisible true)
@@ -51,4 +49,7 @@
     (doto c
       (.setIgnoreRepaint true)
       (.createBufferStrategy 2))
-    (send-off (agent c) run)))
+    (repeatedly #(run (partial draw frame) c))))
+
+(defn -main [& args]
+  (dorun (start)))
