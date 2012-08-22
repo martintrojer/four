615683d50f734c679f3e9057f1c15f7f0c9bb258
Martin Trojer <martin.trojer@gmail.com>
2012-08-22 13:02:31 +0100

    metaballs!

--- src/four.clj
+++ src/four.clj
@@ -1,67 +1,93 @@
+;; https://github.com/yogthos/metaballs
+
 (ns four
-  (import [javax.swing JFrame]
-          [java.awt Color Canvas Dimension])
+  (:import
+   [javax.swing JFrame]
+   [java.awt Canvas Graphics Color]
+   java.awt.image.BufferStrategy)
   (:gen-class))
 
-(def w 800)
-(def h 600)
-(def hz 50)
+(set! *warn-on-reflection* true)
+(def ^:const SIZE 250)
 
-(defn frame [g w h s]
-  (let [c (Color. (rand-int 0xffffff))]
-    (doto g
-      (.setColor c)
-      (.fillRect 0 0 w h)
-      (.setColor Color/WHITE)
-      (.drawString "4k.clj" 20 30))
-    (assoc s :color c)))
+(defn direction [p v]
+  (if (or (> p SIZE) (neg? p )) (- v) v))
 
-(defn timeline [] (repeat frame))
+(defn move [{:keys [x y vx vy radius color]}]
+  (let [vx (direction x vx)
+        vy (direction y vy)]
+    {:x (+ x vx)
+     :y (+ y vy)
+     :vx vx
+     :vy vy
+     :radius radius
+     :color color}))
 
-(defn draw
-  ([c f s]
-     (let [b (.getBufferStrategy c)
-           g (.getDrawGraphics b)
-           [w h] [(.getWidth c) (.getHeight c)]]
-       (try
-         (f g w h (update-in s [:frame] inc))
-         (finally
-          (.dispose g)
-          (when-not (.contentsLost b)
-            (.show b)))))))
+(defn color-in-range [c]
+  (if (< c 0) 0 (if (> c 255) 255 c)))
 
-(defn run [s f]
-  (let [t (System/nanoTime)]
+(defn color [r g b]
+  (new Color 
+       (int (color-in-range r)) 
+       (int (color-in-range g)) 
+       (int (color-in-range b))))
+
+(defn influence [{:keys [x y radius]} px py]
+  (let [dx (double (- x px))
+        dy (double (- y py))]
+    (double (/ radius (Math/sqrt (+ (* dx dx) (* dy dy)))))))
+
+(defn compute-color [x y [red-cur green-cur blue-cur] ball]
+  (let [influence (influence ball x y) 
+        [r g b] (:color ball)]
+    [(+ red-cur (* influence r))
+     (+ green-cur (* influence g))
+     (+ blue-cur (* influence b))]))
+
+(defn paint-square [^Graphics g [red green blue] x y size]
+  (doto g
+      (.setColor ^Color (color red green blue))
+      (.fillRect x y size size)))
+
+(defn draw [^Canvas canvas balls]
+  (let [buffer (.getBufferStrategy canvas)
+        g      (.getDrawGraphics buffer)
+        step   2]
     (try
-      (let [s (f s)
-            d (- (System/nanoTime) t)
-            l (/ 1000000000 hz)]
-        (when (<  d l)
-          (Thread/sleep (/ (- l d) 1000000)))
-        (assoc s :duration d))
-      (catch Exception e
-        (println e)))))
-
-(defn animate [d fs]
-  (map #(partial d %) fs))
-
-(defn canvas []
-  (let [f (doto (JFrame.)
-            (.setResizable false)
-            (.setVisible true)
-            .requestFocus
-            (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE))
-        c (Canvas.)]
-    (doto (.getContentPane f)
-      (.setPreferredSize (Dimension. w h))
-      (.add c))
-    (.pack f)
-    (doto c
-      (.setIgnoreRepaint true)
-      (.createBufferStrategy 2))))
-
-(defn start []
-  (reductions run {:frame 0} (animate (partial draw (canvas)) (timeline))))
+      (loop [x 0]
+        (loop [y 0]
+          (paint-square g (reduce (partial compute-color x y) [0 0 0] balls) x y step)
+          (if (< y (- SIZE step)) (recur (+ y step))))
+        (if (< x (- SIZE step)) (recur (+ x step))))
+      
+      (finally (.dispose g)))
+    (if-not (.contentsLost buffer)
+      (.show buffer)) ))
+ 
+(defn metaball []
+  {:x (rand-int SIZE)
+   :y (rand-int SIZE)
+   :vx (double (inc (rand-int 6)))
+   :vy (double (inc (rand-int 6)))
+   :radius (+ 40 (rand-int 15))
+   :color (take 3 (repeatedly #(rand-int 256)))})
 
 (defn -main [& args]
-  (dorun (start)))
+  (let [frame (JFrame. "Metaballs")
+        canvas (Canvas.)]
+     
+    (doto frame
+      (.setSize SIZE SIZE)
+      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+      (.setResizable false)
+      (.add canvas)
+      (.setVisible true))
+ 
+    (doto canvas
+      (.createBufferStrategy 2)
+      (.setVisible true)
+      (.requestFocus))
+         
+    (loop [balls (take 2 (repeatedly metaball))]
+      (draw canvas balls)
+      (recur (map move balls)))))
