41abca0525891f9aa8b1bc4b14337ac8ea0f0308
Hakan Raberg <hraberg@yahoo.se>
2012-08-27 14:36:13 +0100

    More oscillators

--- test/vlead.clj
+++ test/vlead.clj
@@ -9,16 +9,19 @@
 ;; It cannot do much, try:
 ;; (->> (chord [[:a 4] [:e 4] [:c# 5]] 1/8)
 ;;      (repeat 8)
-;;      (map play)
+;;      (map play))
 
 ;; I don't like the way the time / current frame pollutes the oscillator generation. Early days.
 
 (def bpm 120)
 (def sample-rate 44100)
-(def buffer-size (/ (* sample-rate 2) 50))
-(def ^"[B" buffer (byte-array buffer-size))
-(def sixteen-bit-mono (AudioFormat. sample-rate 16 1 true true))
-(def ^SourceDataLine line-out (AudioSystem/getSourceDataLine sixteen-bit-mono))
+(def audio-format (AudioFormat. sample-rate 16 1 true true))
+
+(def latency 10)
+(def buffer-size (/ (* sample-rate (.getFrameSize audio-format)) (/ 1000 latency)))
+
+(def buffer (byte-array buffer-size))
+(def ^SourceDataLine line-out (AudioSystem/getSourceDataLine audio-format))
 
 (defn frequency [offset-from-a4]
   (* 440.0 (Math/pow 2 (/ offset-from-a4 12.0))))
@@ -35,37 +38,56 @@
   (/ sample-rate hz))
 
 (defn phase [^double period ^long t]
-  (/ t period))
+  (/ (mod t period) period))
 
-(defn sin-osc [^double hz ^long t]
+(defn sin [^double hz ^long t]
   (Math/sin (* (phase (period hz) t) 2.0 Math/PI)))
+
+(defn sqr [^double hz ^long t]
+  (if (< (phase (period hz) t) 0.5) 1.0 -1.0))
+
+(defn tri [^double hz ^long t]
+  (let [phase (phase (period hz) t)]
+    (cond (< phase 0.25) (* phase 4)
+          (< phase 0.75) (* (- phase 0.5) -4)
+          :else (* (- phase 1) 4))))
+
+(defn saw [^double hz ^long t]
+  (let [phase (phase (period hz) t)]
+    (if (< phase 0.5) (* phase 2) (* (- phase 1) 2))))
+
+(defn rnd [_ _]
+  (- (rand 2) 1))
 
 (def seconds-per-beat (/ 60.0 bpm))
 
 (defn tone
   ([] (tone [:a 4]))
-  ([[n oct]] (tone [n oct] 1.0))
-  ([[n oct] length] (tone [n oct] length sin-osc))
-  ([[n oct] length osc]
+  ([note] (tone note 1.0))
+  ([note length] (tone note length sin))
+  ([[n oct & [note-osc vol]] length osc]
      (let [length (* length seconds-per-beat sample-rate)]
        (->> (iterate inc 0)
-            (map (partial osc (note [n oct])))
+            (map (partial (or note-osc osc) (note [n oct])))
+            (map (partial * (or vol 1)))
             (take length)))))
 
 (defn mix [& tracks]
   (/ (apply + tracks) (count tracks)))
 
 (defn chord
-  ([notes length] (chord notes length sin-osc))
+  ([notes length] (chord notes length sin))
   ([notes length osc]
-      (apply map mix (map #(tone % length osc) notes))))
+     (->> notes
+          (map #(tone % length osc))
+          (apply map mix))))
 
 (defn clip [^double sample]
   (min 1.0 (max -1.0 sample)))
 
-(def max-amplitude (dec (Math/pow 2 (dec (.getSampleSizeInBits sixteen-bit-mono)))))
+(def max-amplitude (dec (Math/pow 2 (dec (.getSampleSizeInBits audio-format)))))
 
-(defn sixteen-bit [^double sample]
+(defn bits [^double sample]
   (Math/round (* sample max-amplitude)))
 
 (defn write-sample-byte [^"[B" buffer ^long offset ^long sample]
@@ -88,7 +110,7 @@
 
 (defn play [samples]
   (->> samples
-       (map (comp sixteen-bit clip))
+       (map (comp bits clip))
        (partition (/ buffer-size 2) (/ buffer-size 2) (repeat 0))
        (map (comp out (partial write-sample-buffer buffer)))
        dorun))
@@ -97,12 +119,12 @@
   ([samples] (write "test.wav" samples))
   ([file samples]
      (let [bytes (byte-array (* (count samples) 2))]
-       (write-sample-buffer bytes (map (comp sixteen-bit) samples))
-       (AudioSystem/write (AudioInputStream. (ByteArrayInputStream. bytes) sixteen-bit-mono (count samples))
+       (write-sample-buffer bytes (map (comp bits clip) samples))
+       (AudioSystem/write (AudioInputStream. (ByteArrayInputStream. bytes) audio-format (count samples))
                           AudioFileFormat$Type/WAVE (io/file file)))))
 
 (defn start []
-  (.open line-out sixteen-bit-mono buffer-size)
+  (.open line-out audio-format buffer-size)
   (.start line-out))
 
 (defn stop []
